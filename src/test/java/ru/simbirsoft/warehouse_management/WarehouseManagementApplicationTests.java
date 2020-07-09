1ae4efe090468f0786ad1354c03f502727b35717
Bulat <tynbcx@gmail.com>
2020-07-09 11:57:49 +0300

    init project

---
+++ src/test/java/ru/simbirsoft/warehouse_management/WarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.simbirsoft.warehouse_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
