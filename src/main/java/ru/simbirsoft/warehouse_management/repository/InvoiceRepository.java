68061f37f29b2b63ad12fa5cb8b65a1b84f5c413
Bulat <tynbcx@gmail.com>
2020-07-13 22:56:05 +0300

    remove unnecessary repositories

--- src/main/java/ru/simbirsoft/warehouse_management/repository/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.simbirsoft.warehouse_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.simbirsoft.warehouse_management.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
