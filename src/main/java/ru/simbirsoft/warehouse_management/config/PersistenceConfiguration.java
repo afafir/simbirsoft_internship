b7fdae74d890a6de10f3fe216ab9cfe12e04811d
Bulat <tynbcx@gmail.com>
2020-07-16 22:25:25 +0300

    (FIX) Удалил ненужный конфиг класс c Jpa, добавил SwaggerConfig

--- src/main/java/ru/simbirsoft/warehouse_management/config/PersistenceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ru.simbirsoft.warehouse_management.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories
-public class PersistenceConfiguration {
-}
