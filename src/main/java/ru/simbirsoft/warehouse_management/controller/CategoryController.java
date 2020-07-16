cd03397374bb8cda86a2a6eb45528d7926b1b5bb
Bulat <tynbcx@gmail.com>
2020-07-16 22:31:43 +0300

    (FIX) Создал url.properties, вынес туда все маппинги контроллеров, в самих контроллерах добавил @ApiParam в нужных местах,  DI через конструкторы

--- src/main/java/ru/simbirsoft/warehouse_management/controller/CategoryController.java
+++ src/main/java/ru/simbirsoft/warehouse_management/controller/CategoryController.java
@@ -1,54 +1,61 @@
 package ru.simbirsoft.warehouse_management.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.simbirsoft.warehouse_management.dto.CategoryDto;
 import ru.simbirsoft.warehouse_management.dto.createForms.CategoryCreateDto;
-import ru.simbirsoft.warehouse_management.model.Category;
 import ru.simbirsoft.warehouse_management.service.CategoryService;
-
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@Api("Operations pertaining to categories information if warehouse")
 public class CategoryController {
 
-    @Autowired
-    private CategoryService categoryService;
+  private final CategoryService categoryService;
 
-    @ApiOperation(value = "returns all categories from db")
-    @GetMapping("/categories")
-    private List<CategoryDto> all(){
-        return CategoryDto.from(categoryService.getAllCategories());
-    }
+  @ApiOperation(value = "returns all categories from db")
+  @GetMapping(value = "${url.category}")
+  private List<CategoryDto> all() {
+    return categoryService.getAllCategories();
+  }
 
-    @ApiOperation(value = "returns category with such id.If category doesnt exist, returns custom error message ")
-    @GetMapping("/categories/{id}")
-    private CategoryDto getProduct(@PathVariable Long id){
-        return CategoryDto.from(categoryService.getCategory(id));
-    }
+  @ApiOperation(
+      value =
+          "returns categories with such id.If categories doesnt exist, returns custom error message ")
+  @GetMapping("${url.category}/{id}")
+  private CategoryDto getProduct(@PathVariable @ApiParam("Id of the category") Long id) {
+    return categoryService.getCategory(id);
+  }
 
-    @ApiOperation(value = "Creates new category from dto-form and returns Category entity with id")
-    @PostMapping("/categories/new")
-    private Category newCategory(@RequestBody @Valid CategoryCreateDto categoryCreateDto){
-        return categoryService.createCategory(categoryCreateDto);
-    }
+  @ApiOperation(value = "Creates new categories from dto-form and returns Category entity with id")
+  @PostMapping("${url.category.new}")
+  private CategoryDto newCategory(@RequestBody @Valid CategoryCreateDto categoryCreateDto) {
+    return categoryService.createCategory(categoryCreateDto);
+  }
 
-    @ApiOperation(value = "Delete category from db with such id. If category doesnt exist, returns custom error message")
-    @DeleteMapping("/categories/{id}")
-    private ResponseEntity<String> deleteCategory(@PathVariable Long id){
-        categoryService.deleteCategory(id);
-        return new ResponseEntity<>("Category was deleted successfully",HttpStatus.OK);
-    }
+  @ApiOperation(
+      value =
+          "Delete categories from db with such id. If categories doesnt exist, returns custom error message")
+  @DeleteMapping("${url.category}/{id}")
+  private ResponseEntity<String> deleteCategory(
+      @PathVariable @ApiParam("Id of the category") Long id) {
+    categoryService.deleteCategory(id);
+    return new ResponseEntity<>("Category was deleted successfully", HttpStatus.OK);
+  }
 
-    @ApiOperation(value = "Update category replacing fields in the database with fields from the СategoryDto form." +
-            " If category doesnt exist, returns custom error message")
-    @PutMapping("/categories/update")
-    private CategoryDto updateItem(@RequestBody CategoryDto categoryDto){
-        return CategoryDto.from(categoryService.updateCategory(categoryDto));
-    }
+  @ApiOperation(
+      value =
+          "Update categories replacing fields in the database with fields from the СategoryDto form."
+              + " If categories doesnt exist, returns custom error message")
+  @PutMapping("${url.category.update}")
+  private CategoryDto updateItem(@RequestBody CategoryDto categoryDto) {
+    return categoryService.updateCategory(categoryDto);
+  }
 }
--- src/main/java/ru/simbirsoft/warehouse_management/controller/ItemController.java
+++ src/main/java/ru/simbirsoft/warehouse_management/controller/ItemController.java
@@ -1,53 +1,59 @@
 package ru.simbirsoft.warehouse_management.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.simbirsoft.warehouse_management.dto.ItemDto;
 import ru.simbirsoft.warehouse_management.dto.createForms.ItemCreateDto;
-import ru.simbirsoft.warehouse_management.model.Item;
 import ru.simbirsoft.warehouse_management.service.ItemService;
-
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@Api("Operations pertaining to item information if warehouse")
 public class ItemController {
 
-    @Autowired
-    private ItemService itemService;
+  private final ItemService itemService;
 
-    @ApiOperation(value = "returns all items from db")
-    @GetMapping("/items")
-    private List<ItemDto> all(){
-        return ItemDto.from(itemService.getAllItems());
-    }
+  @ApiOperation(value = "returns all items from db")
+  @GetMapping("${url.item}")
+  private List<ItemDto> all() {
+    return itemService.getAllItems();
+  }
 
-    @ApiOperation(value = "returns item with such id.If category doesnt exist, returns custom error message ")
-    @GetMapping("/items/{id}")
-    private ItemDto getItem(@PathVariable Long id){
-        return ItemDto.from(itemService.getItem(id));
-    }
+  @ApiOperation(
+      value = "returns item with such id.If categories doesnt exist, returns custom error message ")
+  @GetMapping("${url.item}/{id}")
+  private ItemDto getItem(@PathVariable @ApiParam("Id of the item") Long id) {
+    return itemService.getItem(id);
+  }
 
-    @ApiOperation(value = "Creates new Item from dto-form and returns Item entity with id")
-    @PostMapping("/items/new")
-    private Item newItem(@RequestBody @Valid ItemCreateDto itemCreateDto){
-        return itemService.createItem(itemCreateDto);
-    }
+  @ApiOperation(value = "Creates new Item from dto-form and returns Item entity with id")
+  @PostMapping("${url.item.new}")
+  private ItemDto newItem(@RequestBody @Valid ItemCreateDto itemCreateDto) {
+    return itemService.createItem(itemCreateDto);
+  }
 
-    @ApiOperation(value = "Delete Item from db with such id. If category doesnt exist, returns custom error message")
-    @DeleteMapping("/items/{id}")
-    private ResponseEntity<String> deleteItem(@PathVariable Long id){
-        itemService.deleteItem(id);
-        return new ResponseEntity<>("Item was deleted successfully", HttpStatus.OK);
-    }
+  @ApiOperation(
+      value =
+          "Delete Item from db with such id. If categories doesnt exist, returns custom error message")
+  @DeleteMapping("${url.item}/{id}")
+  private ResponseEntity<String> deleteItem(@PathVariable @ApiParam("Id of the item") Long id) {
+    itemService.deleteItem(id);
+    return new ResponseEntity<>("Item was deleted successfully", HttpStatus.OK);
+  }
 
-    @ApiOperation(value = "Update item replacing fields in the database with fields from the ItemDto form." +
-            " If item or new category of this item doesnt exist in db, returns custom error message")
-    @PutMapping("/items/update")
-    private ItemDto updateItem(@RequestBody ItemDto itemDto){
-        return ItemDto.from(itemService.updateItem(itemDto));
-    }
+  @ApiOperation(
+      value =
+          "Update item replacing fields in the database with fields from the ItemDto form."
+              + " If item or new categories of this item doesnt exist in db, returns custom error message")
+  @PutMapping("${url.item.update}")
+  private ItemDto updateItem(@RequestBody ItemDto itemDto) {
+    return itemService.updateItem(itemDto);
+  }
 }
