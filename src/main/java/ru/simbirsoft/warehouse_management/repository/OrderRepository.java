f4e194c9ae1aa85ad83ccd8e5ae3a38e30dfc58d
Bulat <tynbcx@gmail.com>
2020-08-13 22:27:16 +0300

    OrderRepository новый метод для возвращения заказов, совершенных в определенное время или в определенное время в конкретном магазине

--- src/main/java/ru/simbirsoft/warehouse_management/repository/OrderRepository.java
+++ src/main/java/ru/simbirsoft/warehouse_management/repository/OrderRepository.java
@@ -1,15 +1,17 @@
 package ru.simbirsoft.warehouse_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.simbirsoft.warehouse_management.dto.OrderDto;
 import ru.simbirsoft.warehouse_management.model.Order;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     boolean existsByCustomerIdAndIsConfirmedFalse(Long userId);
     List<Order> findByCustomerId(Long userId);
+    List<Order> findByOrderedAtBetweenAndIsConfirmedTrue(LocalDateTime start, LocalDateTime end);
+    List<Order> findByOrderedAtBetweenAndShopIdAndIsConfirmedTrue(LocalDateTime start, LocalDateTime end, Long id);
     Optional<Order> findByCustomerIdAndIsConfirmedFalse(Long userId);
 
 }
