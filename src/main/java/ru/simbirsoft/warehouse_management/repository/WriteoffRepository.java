e6861a773d6016dfce4c6c2fa14a4632dfffffd2
Bulat <tynbcx@gmail.com>
2020-08-13 22:28:26 +0300

    WriteoffRepository Новые методы для поиска списаний в определенный период времени или в определнный период времени для определенного склада

--- src/main/java/ru/simbirsoft/warehouse_management/repository/WriteoffRepository.java
+++ src/main/java/ru/simbirsoft/warehouse_management/repository/WriteoffRepository.java
@@ -3,8 +3,12 @@ package ru.simbirsoft.warehouse_management.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.simbirsoft.warehouse_management.model.Writeoff;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface WriteoffRepository extends JpaRepository<Writeoff, Long> {
   List<Writeoff> findByConfirmedFalse();
+  List<Writeoff> findByTimeBetweenAndConfirmedTrue(LocalDateTime start, LocalDateTime end);
+  List<Writeoff> findByTimeBetweenAndWarehouseIdAndConfirmedTrue(LocalDateTime start, LocalDateTime end, Long warehouseId);
+
 }
